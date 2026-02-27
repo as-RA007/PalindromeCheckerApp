03ed19be770c5461181db1ce82b6aeedcf752abf
as-RA007 <as9098@srmist.edu.in>
2026-02-27 14:55:22 +0530

    UC4: Add case insensitive palindrome

---
+++ src/UC3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class UC3 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.print("Enter a word: ");
+
+        String word = scanner.nextLine();
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+
+        scanner.close();
+    }
+}
